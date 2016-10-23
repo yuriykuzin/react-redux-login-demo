4879aa12b3a8d6fc1597d1f1c94bf0a69de18574
Yuriy <yuriy.kuzin@gmail.com>
2016-10-23 20:14:25 +0000

    Upd server.js to fit Heroku restrictions. Now it runs without MockServer

--- server.js
+++ server.js
@@ -1,114 +1,55 @@
 var http = require('http');
 var static = require('node-static');
 var file = new static.Server('./build');
-var mockserver = require('mockserver-grunt');
-var mockServer = require('mockserver-client');
-var mockServerClient = mockServer.mockServerClient;
 
 var portMockServer = process.env.PORT || 8080;
-var ipMockServer = process.env.IP || '0.0.0.0';
 
 http.createServer(function(req, res) {
-  file.serve(req, res);
-}).listen(8082);
+  var body = '';
+  var resLogin = 'Denied';
+  var reqLogin;
 
-console.log('Static server is running on port 8082');
-
-mockserver.start_mockserver({
-  serverPort: portMockServer,
-  verbose: true
-});
-
-console.log('Mock server is running on port ' + portMockServer);
-console.log('IP address is ' + ipMockServer);
-
-setTimeout(function() {
-
-  mockServerClient(ipMockServer, portMockServer).mockAnyResponse({
-    'httpRequest': {
-      'method': 'GET'
-    },
-    "httpForward": {
-      "host": "0.0.0.0",
-      "port": 8082,
-      "scheme": "HTTP"
-    },
-    'times': {
-      'unlimited': true
-    }
-  });
-  
-  mockServerClient(ipMockServer, portMockServer).mockAnyResponse({
-    "httpRequest": {
-      "method": "POST",
-      "path": "/login",
-      "body": {
-        "type": "JSON",
-        "value": JSON.stringify({
-          Username: "User",
-          Password: "Password"
-        }),
-        "matchType": "STRICT"
+  if (req.url === '/login' && req.method === 'POST') {
+    req.on('data', function(data) {
+      body += data;
+      
+      // Standard procedure to preventing flood request
+      if (body.length > 1e6) {
+        body = '';
+        res.writeHead(413, {
+          'Content-Type': 'text/plain'
+        }).end();
+        req.connection.destroy();
       }
-    },
-    "httpResponse": {
-      "statusCode": 200,
-      "headers": [{
-        "name": "Content-Type",
-        "values": ["application/json; charset=utf-8"]
-      }, {
-        "name": "Cache-Control",
-        "values": ["public, max-age=86400"]
-      }],
-      "body": JSON.stringify({
-        Auth: "Logged",
-        Language: "EN"
-      }),
-      "delay": {
-        "timeUnit": "SECONDS",
-        "value": 1
+    });
+    
+    req.on('end', function() {
+      try {
+        var reqLogin = JSON.parse(body);
+        if (reqLogin && reqLogin.Username === 'User' && reqLogin.Password === 'Password') {
+          resLogin = 'Logged';
+        }
+      } catch (err) {
+        console.log(err);
       }
-    },
-    "times": {
-      "unlimited": true
-    }
-  });   
 
-  mockServerClient(ipMockServer, portMockServer).mockAnyResponse({
-    "httpRequest": {
-      "method": "POST",
-      "path": "/login",
-      "body": {
-        "type": "JSON",
-        "value": JSON.stringify({
-          Username: "foo",
-          Password: "bar"
-        }),
-        "matchType": "STRICT"
-      }
-    },
-    "httpResponse": {
-      "statusCode": 200,
-      "headers": [{
-        "name": "Content-Type",
-        "values": ["application/json; charset=utf-8"]
-      }, {
-        "name": "Cache-Control",
-        "values": ["public, max-age=86400"]
-      }],
-      "body": JSON.stringify({
-        Auth: "Denied"
-      }),
-      "delay": {
-        "timeUnit": "SECONDS",
-        "value": 1
-      }
-    },
-    "times": {
-      "unlimited": true
-    }
-  });
+      setTimeout(function() {
+        res.writeHead(200, {
+          'Content-Type': 'application/json; charset=utf-8',
+          'Cache-Control': 'public, max-age=86400'
+        });
+        res.write(JSON.stringify({
+          Auth: resLogin,
+          Language: "EN"
+        }));
+        res.end();
+      }, 1000);
+    });
+  } else {
+    
+    // Serving static files
+    file.serve(req, res);
+  }
+}).listen(portMockServer);
 
-  console.log("Mock server initiated");
-
-}, 5000);
+console.log('Server is running on port ' + portMockServer);
